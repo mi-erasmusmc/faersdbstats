a78ca9d083fce2403e89d054c11b1e02bd9fbab7
Rowan <r.parry@erasmusmc.nl>
2021-04-06 11:26:10 +0200

    remove unnessary line from create deleted file script

--- load_data_files_from_website/create_deleted_data_file.sh
+++ load_data_files_from_website/create_deleted_data_file.sh
@@ -6,6 +6,3 @@ cd deleted || exit
 awk 1 ./*.txt >temp.txt
 tr -d '\015' <temp.txt >all_deleted_cases.txt
 rm temp.txt
-
-
-psql -h 127.0.0.1 -U postgres -c "\COPY unique_all_case ~/Desktop/faers/exports/unique_all_case_dollar.csv WITH DELIMITER E'$' HEADER CSV" cem
